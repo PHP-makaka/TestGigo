04d85bfe7229e109e0f1cb0bc4427026460f3ceb
Your Name <gurkov1999@mail.ru>
2021-03-03 19:51:43 +0300

    wqd

--- js.js
+++ js.js
@@ -3,6 +3,6 @@ window.addEventListener("deviceorientation", orientation, true);
 function orientation(event) {
   var x = event.beta;
   var y = event.gamma;
-  
+ console.log("!!1"+x) 
 alert(x,y)
 }
